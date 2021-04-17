26d8c5dcb6d2291c0a68bbbe9794885d17af047f
Banji <banji.adeyemi@yahoo.com>
2021-04-16 20:47:55 -0400

    Check in 1

--- FinalProjectCheckIn1.py
+++ FinalProjectCheckIn1.py
@@ -1,1 +1,31 @@
-print("Hello") 
+Class Premiere League
+""" A script to show what player was on top in the premiere league in the goals and assists category for the 2019/2020 season.
+This script will show the top players in each of the categories listed above.
+"""
+
+ Def Total Goals
+   """ This function will return a total of the goals scored by each individual player in the league.
+    
+    Args: 
+        total_goals: total goals scored by each player.
+        
+    Returns:
+        total_goals: stores a dataframe of total goals scored by each player.
+    """
+
+ Def Total Assists
+    """ This function will return a total of the assists made by each individual player in the league.
+
+    Args:
+        total_assists: total assists made by each player.
+
+    Returns:
+        total_assists: stores a dataframe of total assists made by each player.
+    """
+
+Def Total Stats
+    """ This function shows the goals and assists mixed together in one dataframe named "total_stats".
+
+    Returns:
+        total_stats: A dataframe of total stats (goals and assists merged together) for each player.
+    """
